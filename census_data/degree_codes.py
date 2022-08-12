19efcecd1499bd1b565fb7fbe77f10809b377cb7
Filipp Krasovsky <fkrasovsky@allvuesystems.com>
2022-08-12 01:13:48 -0700

    finish optimization, restructure directory tree

--- census_data/degree_codes.py
+++ census_data/degree_codes.py
@@ -55,60 +55,61 @@ fips = {
     ,72:"Puerto Rico",97:"Military/Mil. Reservation",99:"State not identified"
 }
 
+#specific name of degree
 detail_codes={
 0: "N/A"
 
-    ,1100: "General Agriculture" 
+,1100: "General Agriculture" 
 
 ,1101: "Agriculture Production and Management" ,1102: "Agricultural Economics" 
 
-    ,1103: "Animal Sciences",1104: "Food Science" ,1105: "Plant Science and Agronomy" ,1106: "Soil Science" 
-    ,1199: "Miscellaneous Agriculture" ,1300: "Environment and Natural Resources",1301: "Environmental Science" ,1302: "Forestry" 
-    ,1303: "Natural Resources Management" 
+,1103: "Animal Sciences",1104: "Food Science" ,1105: "Plant Science and Agronomy" ,1106: "Soil Science" 
+,1199: "Miscellaneous Agriculture" ,1300: "Environment and Natural Resources",1301: "Environmental Science" ,1302: "Forestry" 
+,1303: "Natural Resources Management" 
 
 ,1401: "Architecture" ,1501: "Area, Ethnic, and Civilization Studies"
 ,1900: "Communications",1901: "Communications" ,1902: "Journalism",1903: "Mass Media",1904: "Advertising and Public Relations",2001: "Communication Technologies"
-    
-    ,2100: "Computer and Information Systems",2101: "Computer Programming and Data Processing",2102: "Computer Science"
-    ,2105: "Information Sciences"
-    ,2106: "Computer Information Management and Security"
+
+,2100: "Computer and Information Systems",2101: "Computer Programming and Data Processing",2102: "Computer Science"
+,2105: "Information Sciences"
+,2106: "Computer Information Management and Security"
 
 ,2107: "Computer Networking and Telecommunications",2201: "Cosmetology Services and Culinary Arts",2300: "General Education",2301: "Educational Administration and Supervision"
 ,2303: "School Student Counseling",2304: "Elementary Education",2305: "Mathematics Teacher Education",2306: "Physical and Health Education Teaching"
 ,2307: "Early Childhood Education",2308: "Science and Computer Teacher Education",2309: "Secondary Teacher Education"
 ,2310: "Special Needs Education",2311: "Social Science or History Teacher Education",2312: "Teacher Education: Multiple Levels"
 ,2313: "Language and Drama Education",2314: "Art and Music Education",2399: "Miscellaneous Education"
-    
-    ,2400: "General Engineering",2401: "Aerospace Engineering",2402: "Biological Engineering",2403: "Architectural Engineering",2404: "Biomedical Engineering"
-    ,2405: "Chemical Engineering",2406: "Civil Engineering",2407: "Computer Engineering",2408: "Electrical Engineering",2409: "Engineering Mechanics, Physics, and Science"
-    ,2410: "Environmental Engineering",2411: "Geological and Geophysical Engineering",2412: "Industrial and Manufacturing Engineering",2413: "Materials Engineering and Materials Science"
-    ,2414: "Mechanical Engineering",2415: "Metallurgical Engineering",2416: "Mining and Mineral Engineering",2417: "Naval Architecture and Marine Engineering"
-    ,2418: "Nuclear Engineering",2419: "Petroleum Engineering",2499: "Miscellaneous Engineering",2500: "Engineering Technologies"
-    ,2501: "Engineering and Industrial Management"
-    ,2502: "Electrical Engineering Technology"
-    ,2503: "Industrial Production Technologies",2504: "Mechanical Engineering Related Technologies"
-    ,2599: "Miscellaneous Engineering Technologies"
+
+,2400: "General Engineering",2401: "Aerospace Engineering",2402: "Biological Engineering",2403: "Architectural Engineering",2404: "Biomedical Engineering"
+,2405: "Chemical Engineering",2406: "Civil Engineering",2407: "Computer Engineering",2408: "Electrical Engineering",2409: "Engineering Mechanics, Physics, and Science"
+,2410: "Environmental Engineering",2411: "Geological and Geophysical Engineering",2412: "Industrial and Manufacturing Engineering",2413: "Materials Engineering and Materials Science"
+,2414: "Mechanical Engineering",2415: "Metallurgical Engineering",2416: "Mining and Mineral Engineering",2417: "Naval Architecture and Marine Engineering"
+,2418: "Nuclear Engineering",2419: "Petroleum Engineering",2499: "Miscellaneous Engineering",2500: "Engineering Technologies"
+,2501: "Engineering and Industrial Management"
+,2502: "Electrical Engineering Technology"
+,2503: "Industrial Production Technologies",2504: "Mechanical Engineering Related Technologies"
+,2599: "Miscellaneous Engineering Technologies"
 
 ,2600: "Linguistics and Foreign Languages",2601: "Linguistics and Comparative Language and Literature",2602: "French, German, Latin and Other Common Foreign Language Studies"
 ,2603: "Other Foreign Languages",2901: "Family and Consumer Sciences",3200: "Law",3201: "Court Reporting",3202: "Pre-Law and Legal Studies",3300: "English Language, Literature, and Composition"
 ,3301: "English Language and Literature",3302: "Composition and Speech",3400: "Liberal Arts and Humanities",3401: "Liberal Arts",3402: "Humanities",3501: "Library Science"
-    
-    ,3600: "Biology",3601: "Biochemical Sciences",3602: "Botany",3603: "Molecular Biology",3604: "Ecology",3605: "Genetics",3606: "Microbiology",3607: "Pharmacology",3608: "Physiology"
-    ,3609: "Zoology",3611: "Neuroscience",3699: "Miscellaneous Biology",3700: "Mathematics",3701: "Applied Mathematics",3702: "Statistics and Decision Science",3801: "Military Technologies"
+
+,3600: "Biology",3601: "Biochemical Sciences",3602: "Botany",3603: "Molecular Biology",3604: "Ecology",3605: "Genetics",3606: "Microbiology",3607: "Pharmacology",3608: "Physiology"
+,3609: "Zoology",3611: "Neuroscience",3699: "Miscellaneous Biology",3700: "Mathematics",3701: "Applied Mathematics",3702: "Statistics and Decision Science",3801: "Military Technologies"
 
 ,4000: "Interdisciplinary and Multi-Disciplinary Studies (General)",4001: "Intercultural and International Studies"
-    
-    ,4002: "Nutrition Sciences"
-    ,4003: "Neuroscience"
-    ,4005: "Mathematics and Computer Science"
+
+,4002: "Nutrition Sciences"
+,4003: "Neuroscience"
+,4005: "Mathematics and Computer Science"
 
 ,4006: "Cognitive Science and Biopsychology"
 ,4007: "Interdisciplinary Social Sciences",4008: "Multi-disciplinary or General Science",4101: "Physical Fitness, Parks, Recreation, and Leisure"
 ,4801: "Philosophy and Religious Studies",4901: "Theology and Religious Vocations"
 
-    ,5000: "Physical Sciences",5001: "Astronomy and Astrophysics",5002: "Atmospheric Sciences and Meteorology",5003: "Chemistry"
-    ,5004: "Geology and Earth Science",5005: "Geosciences",5006: "Oceanography",5007: "Physics",5008: "Materials Science",5098: "Multi-disciplinary or General Science"
-    ,5102: "Nuclear, Industrial Radiology, and Biological Technologies"
+,5000: "Physical Sciences",5001: "Astronomy and Astrophysics",5002: "Atmospheric Sciences and Meteorology",5003: "Chemistry"
+,5004: "Geology and Earth Science",5005: "Geosciences",5006: "Oceanography",5007: "Physics",5008: "Materials Science",5098: "Multi-disciplinary or General Science"
+,5102: "Nuclear, Industrial Radiology, and Biological Technologies"
 
 ,5200: "Psychology",5201: "Educational Psychology",5202: "Clinical Psychology",5203: "Counseling Psychology",5205: "Industrial and Organizational Psychology"
 ,5206: "Social Psychology",5299: "Miscellaneous Psychology",5301: "Criminal Justice and Fire Protection",5400: "Public Affairs, Policy, and Social Work",5401: "Public Administration"
@@ -134,14 +135,14 @@ detail_codes={
 ,6104: "Medical Assisting Services"
 ,6105: "Medical Technologies Technicians"
 ,6106: "Health and Medical Preparatory Programs"
-    ,6107: "Nursing"
-    ,6108: "Pharmacy, Pharmaceutical Sciences, and Administration"
+,6107: "Nursing"
+,6108: "Pharmacy, Pharmaceutical Sciences, and Administration"
 ,6109: "Treatment Therapy Professions"
 ,6110: "Community and Public Health"
 ,6199: "Miscellaneous Health Medical Professions"
 ,6200: "General Business"
 ,6201: "Accounting"
-    ,6202: "Actuarial Science"
+,6202: "Actuarial Science"
 ,6203: "Business Management and Administration"
 ,6204: "Operations, Logistics and E-Commerce"
 ,6205: "Business Economics"
@@ -150,27 +151,28 @@ detail_codes={
 ,6209: "Human Resources and Personnel Management"
 ,6210: "International Business"
 ,6211: "Hospitality Management"
-    ,6212: "Management Information Systems and Statistics"
+,6212: "Management Information Systems and Statistics"
 ,6299: "Miscellaneous Business and Medical Administration"
 ,6402: "History"
 ,6403: "United States History"
 }
 
+#degree area codes
 degree_codes={
-    0:"N/A",11:"Agriculture",13:"Environment and Natural Resources",14:"Architecture"
-    ,15:"Area, Ethnic, and Civilization Studies",19:"Communications",20:"Communication Technologies"
-    ,21:"Computer and Information Sciences",22:"Cosmetology Services and Culinary Arts"
-    ,23:"Education Administration and Teaching",24:"Engineering",25:"Engineering Technologies"
-    ,26:"Linguistics and Foreign Languages",29:"Family and Consumer Sciences",32:"Law"
-    ,33:"English Language, Literature, and Composition",34:"Liberal Arts and Humanities"
-    ,35:"Library Science",36:"Biology and Life Sciences",37:"Mathematics and Statistics"
-    ,38:"Military Technologies",40:"Interdisciplinary and Multi-Disciplinary Studies (General)"
-    ,41:"Physical Fitness, Parks, Recreation, and Leisure",48:"Philosophy and Religious Studies"
-    ,49:"Theology and Religious Vocations",50:"Physical Sciences",51:"Nuclear, Industrial Radiology, and Biological Technologies"
-    ,52:"Psychology",53:"Criminal Justice and Fire Protection",54:"Public Affairs, Policy, and Social Work"
-    ,55:"Social Sciences",56:"Construction Services",57:"Electrical and Mechanic Repairs and Technologies"
-    ,58:"Precision Production and Industrial Arts",59:"Transportation Sciences and Technologies"
-    ,60:"Fine Arts",61:"Medical and Health Sciences and Services",62:"Business",64:"History"
+0:"N/A",11:"Agriculture",13:"Environment and Natural Resources",14:"Architecture"
+,15:"Area, Ethnic, and Civilization Studies",19:"Communications",20:"Communication Technologies"
+,21:"Computer and Information Sciences",22:"Cosmetology Services and Culinary Arts"
+,23:"Education Administration and Teaching",24:"Engineering",25:"Engineering Technologies"
+,26:"Linguistics and Foreign Languages",29:"Family and Consumer Sciences",32:"Law"
+,33:"English Language, Literature, and Composition",34:"Liberal Arts and Humanities"
+,35:"Library Science",36:"Biology and Life Sciences",37:"Mathematics and Statistics"
+,38:"Military Technologies",40:"Interdisciplinary and Multi-Disciplinary Studies (General)"
+,41:"Physical Fitness, Parks, Recreation, and Leisure",48:"Philosophy and Religious Studies"
+,49:"Theology and Religious Vocations",50:"Physical Sciences",51:"Nuclear, Industrial Radiology, and Biological Technologies"
+,52:"Psychology",53:"Criminal Justice and Fire Protection",54:"Public Affairs, Policy, and Social Work"
+,55:"Social Sciences",56:"Construction Services",57:"Electrical and Mechanic Repairs and Technologies"
+,58:"Precision Production and Industrial Arts",59:"Transportation Sciences and Technologies"
+,60:"Fine Arts",61:"Medical and Health Sciences and Services",62:"Business",64:"History"
 }
 
 stem_degree_codes = [11,13,21,24,25,36,37,50,51,57,61]
